7ae378dbc86d35e78875187b9aa97d96de20ca1b
jcepriano <jamescepriano@gmail.com>
2023-08-21 12:27:53 -0700

    speedrun

---
+++ CaravanMVC/Models/Passenger.cs
@@ -0,0 +1,11 @@
+﻿namespace CaravanMVC.Models
+{
+    public class Passenger
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Destination { get; set; }
+        public Wagon Wagon { get; set; }
+    }
+}
