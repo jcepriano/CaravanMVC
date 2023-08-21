e09734a3f4ece5e8394802a14f6132c3690c84e8
jcepriano <jamescepriano@gmail.com>
2023-08-21 13:25:56 -0700

    user stories 1 and 2

---
+++ CaravanMVC/Controllers/WagonsController.cs
@@ -0,0 +1,30 @@
+﻿using CaravanMVC.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaravanMVC.Controllers
+{
+    public class WagonsController : Controller
+    {
+        private readonly CaravanMvcContext _context;
+
+        public WagonsController(CaravanMvcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+           var wagons = _context.Wagons.ToList();
+
+            return View(wagons);
+        }
+
+        [Route("wagons/{id:int}")]
+        public IActionResult Show(int id)
+        {
+            var wagon = _context.Wagons.Find(id);
+
+            return View(wagon);
+        }
+    }
+}
